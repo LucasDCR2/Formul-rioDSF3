b95c7c6dc28af6bdd8d708824109109083248964
Lucas Rodrigues PC <lucasrodrigues2606@gmail.com>
2023-05-09 15:53:58 -0300

    firsttry

--- dados.php
+++ dados.php
@@ -1,54 +1,28 @@
-<?php 
+<?php
 
-	// Recebe os dados do formulário
-	$nome = $_POST['nome'];
-	$email = $_POST['email'];
-	$cpf = $_POST['cpf'];
-	$genero = $_POST['genero'];
+// Recebe os dados do formulário
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$cpf = $_POST['cpf'];
+$genero = $_POST['genero'];
 
-	// Validação dos dados
-	$erros = array();
+// Valida os dados do formulário
+if (empty($nome) || empty($email) || empty($cpf) || empty($genero)) {
+    echo 'Por favor, preencha todos os campos.';
 
-	if (empty($nome)) {
-		$erros[] = "O campo nome é obrigatório.";
-	}
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo 'Por favor, informe um endereço de e-mail válido.';
 
-	if (empty($email)) {
-		$erros[] = "O campo e-mail é obrigatório.";
-	} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		$erros[] = "O e-mail informado é inválido.";
-	}
+} elseif (!preg_match('/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/', $cpf)) {
+    echo 'Por favor, informe um CPF válido.';
 
+} else {
+    // Cria uma nova string com os dados do formulário
+    $newData = "Nome: $nome\nEmail: $email\nCPF: $cpf\nGênero: $genero\n\n";
 
+    // Salva a string no arquivo de texto
+    file_put_contents('data.txt', $newData, FILE_APPEND);
 
-	if (empty($cpf)) {
-		$erros[] = "O campo CPF é obrigatório.";
-	} else if (!preg_match('/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/', $cpf)) {
-		$erros[] = "O CPF informado é inválido.";
-	}else {
-		// Grava os dados em um arquivo
-	$dados = array(
-		'nome' => $nome,
-		'email' => $email,
-		'cpf' => $cpf,
-		'genero' => $genero
-	);
-	$json = json_encode($dados);
-	file_put_contents('dados.json', $json);
-	
-
-		// Mostra alerta de sucesso
-	echo "<div>Dados enviados com sucesso!</div>";
-	}
-
-
-		
-
-		// Mostra alerta de sucesso
-		/*<div id="alerta" class="alert"></div>
-		echo "<p class='alert alert-success'>Dados enviados com sucesso!</p>";
-		*/
-  
-?>
-
+    echo 'Dados enviados com sucesso!';
+}
 
--- index.php
+++ index.php
@@ -14,26 +14,31 @@
 	<!--<script src="backend.js"></script> -->
 </head>
 <body>
-	<form action="dados.php" method="POST">
-		<label for="nome">Nome:</label>
-		<input type="text" id="nome" placeholder = "Digite seu Nome" name="nome" required>
+<script src="backend.js"></script>
+	<section class = "section">
+		<div class = "divisao">
+			<form id = "form" action="dados.php" method="POST">
+				<label for="nome">Nome:</label>
+				<input type="text" id="nome" placeholder = "Digite seu Nome" name="nome" required>
 
-		<label for="email">E-mail:</label>
-		<input type="email" id="email" placeholder = "Digite seu E-mail" name="email" required>
+				<label for="email">E-mail:</label>
+				<input type="email" id="email" placeholder = "Digite seu E-mail" name="email" required>
 
-		<label for="cpf">CPF:</label>
-		<input type="text" id="cpf" placeholder = "Digite seu CPF" name="cpf" size="14" maxlength="14" oninput="this.value = this.value.replace(/[^-,0-9,.]/g, '');"required>
+				<label for="cpf">CPF:</label>
+				<input type="text" id="cpf" placeholder = "Digite seu CPF" name="cpf" size="14" maxlength="14" oninput="this.value = this.value.replace(/[^-,0-9,.]/g, '');"required>
 
-		<label for="genero">Gênero:</label>
-		<select id="genero" name="genero" required>
-			<option value="">Selecione</option>
-			<option value="masculino">Masculino</option>
-			<option value="feminino">Feminino</option>
-			<option value="outro">Outro</option>
-		</select>
+				<label for="genero">Gênero:</label>
+				<select id="genero" name="genero" required>
+					<option value="">Selecione</option>
+					<option value="masculino">Masculino</option>
+					<option value="feminino">Feminino</option>
+					<option value="outro">Outro</option>
+				</select>
 
-		<button type="submit">Enviar</button>
-		
-	</form>
-
-	
+				<button type="submit">Enviar</button>
+				
+			</form>
+			<script src="script.js"></script>
+		</div>
+	</section>
+</body>
